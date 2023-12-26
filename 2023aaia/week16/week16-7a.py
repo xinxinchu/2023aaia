6199628441a89f2d2289cc5738d6a1be6df74cca
xinxinchu <12750706@me.mcu.edu.tw>
2023-12-26 11:47:27 +0800

    week16

---
+++ 2023aaia/week16/week16-7a.py
@@ -0,0 +1,3 @@
+a = list(map(int,input().split() ))
+a.sort()
+print(a[0] + a[1]*10 + a[2]*100+1, end='')
---
+++ 2023aaia/week16/week16-7b.py
@@ -0,0 +1,6 @@
+a = int(input() )
+
+ss = a%60
+mm = a//60%60
+hh = a//60//60
+print(f'{hh:02}:{mm:02}:{ss:02}',end='')
